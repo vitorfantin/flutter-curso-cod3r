cf9830580788150d9163a623348bba8272f7616b
Vitor Fantin <96424825+vitorfantin@users.noreply.github.com>
2022-12-08 03:32:51 -0300

    first commit

---
+++ aulas.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  const double pi = 3.1415;
+  print('Digite valor do raio 1: ');
+  final String? entradaRaio1 = stdin.readLineSync();
+  final double raio1 = double.parse(entradaRaio1!);
+  print('Digite valor do Raio 2: ');
+  final String? entradaRaio2 = stdin.readLineSync();
+  final double raio2 = double.parse(entradaRaio2!);
+  final double resultado = pi * (raio1 * raio2);
+
+  print('O valor da circunferencia é de: $resultado');
+}
